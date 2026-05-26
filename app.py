895c016a625ef658b5ec9f405dd12dbd6aa0bd44
vvanhogjxa-png <vvanhogjxa@gmail.com>
2026-05-26 11:20:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -153,4 +153,4 @@ def export(job_id):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
